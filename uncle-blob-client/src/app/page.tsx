5b439e8a47242f7fbd2c4639a4868ea4a5f44231
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-02-04 00:11:14 +0200

    adds some stuff on main page

--- uncle-blob-client/src/app/page.tsx
+++ uncle-blob-client/src/app/page.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function HomePage() {
   return (
-    <div className='p-10' >Main Page</div>
+    <div className='p-10 flex flex-col items-center justify-center text-center' >
+      <h1 className='text-4xl md:text-7xl font-black' >Uncle Blob Client</h1>
+
+      <p className='text-slate-500 mt-3' >Client UI for the easy to use light weight file server</p>
+    </div>
   )
 }
