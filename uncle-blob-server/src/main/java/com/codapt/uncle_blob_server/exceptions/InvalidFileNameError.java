c916af1faaebd0acba0ca3913f6d6d4fe80bda38
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-02-03 14:21:04 +0200

    invalid file name error

--- uncle-blob-server/src/main/java/com/codapt/uncle_blob_server/exceptions/InvalidFileNameError.java
+++ uncle-blob-server/src/main/java/com/codapt/uncle_blob_server/exceptions/InvalidFileNameError.java
@@ -1,9 +1,16 @@
 package com.codapt.uncle_blob_server.exceptions;
 
-public class InvalidFileNameError extends RuntimeException {
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+
+@Getter
+public class InvalidFileNameError extends RestRuntimeException {
     
     private String error;
     private String message;
+    
+    private HttpStatus status = HttpStatus.BAD_REQUEST;
 
     public InvalidFileNameError(String message) {
         this.error = "Invalid File Name";
