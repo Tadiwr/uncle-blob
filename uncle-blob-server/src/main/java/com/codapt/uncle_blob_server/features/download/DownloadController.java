32310c021f339037ec863f82e4a14f8da0d00da2
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-02-03 13:38:00 +0200

    fixes annotations

--- uncle-blob-server/src/main/java/com/codapt/uncle_blob_server/features/download/DownloadController.java
+++ uncle-blob-server/src/main/java/com/codapt/uncle_blob_server/features/download/DownloadController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
@@ -12,9 +12,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class DownloadController {
     
     @GetMapping("/{fileName}")
-    public ResponseEntity<byte[]> getByFileName(@RequestParam String fileName) {
+    public ResponseEntity<byte[]> getByFileName(@PathVariable String fileName) {
         throw new UnsupportedOperationException();
     }
     
-
 }
