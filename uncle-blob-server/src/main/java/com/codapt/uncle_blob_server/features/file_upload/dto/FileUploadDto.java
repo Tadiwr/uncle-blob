cac3c57b5c6c5bb7446c7023db219b88dcca1466
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-02-03 12:54:05 +0200

    adds upload file api

---
+++ uncle-blob-server/src/main/java/com/codapt/uncle_blob_server/features/file_upload/dto/FileUploadDto.java
@@ -0,0 +1,35 @@
+package com.codapt.uncle_blob_server.features.file_upload.dto;
+
+import java.util.Date;
+
+import com.codapt.uncle_blob_server.features.file_upload.FileUpload;
+
+import lombok.Data;
+
+@Data
+public class FileUploadDto {
+    
+    private String uuid;
+
+    private String fileName;
+    private String originalFileName;
+    
+    private String fileSize;
+    private String contentType;
+
+    private Date uploadedAt;
+    private Date lastUpdatedAt;
+
+    public FileUploadDto(FileUpload file) {
+        this.uuid = file.getUuid();
+        this.fileName = file.getFileName();
+
+        this.contentType = file.getContentType().toString();
+        this.fileSize = file.getFileSize().toString() + " bytes";
+        this.originalFileName = file.getOriginalFileName();
+
+        this.uploadedAt = new Date(file.getUploadedAt());
+        this.lastUpdatedAt =  new Date(file.getLastUpdatedAt());
+    }
+
+}
---
+++ uncle-blob-server/src/main/java/com/codapt/uncle_blob_server/features/upload/UploadController.java
@@ -0,0 +1,27 @@
+package com.codapt.uncle_blob_server.features.upload;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codapt.uncle_blob_server.features.file_upload.dto.FileUploadDto;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/api/v1/files/upload")
+public class UploadController {
+    
+    @PostMapping("")
+    public ResponseEntity<FileUploadDto> uploadFile(
+        @RequestParam String fileName,
+        @RequestBody byte[] fileBytes
+    ) {
+        throw new UnsupportedOperationException();
+    }
+    
+
+}
