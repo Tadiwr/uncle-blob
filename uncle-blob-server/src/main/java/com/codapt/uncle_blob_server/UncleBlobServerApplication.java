cd530d196bffa8bb5fd4824c3010b572938664d5
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-02-03 11:48:37 +0200

    adds uncle blob server

---
+++ uncle-blob-server/src/main/java/com/codapt/uncle_blob_server/UncleBlobServerApplication.java
@@ -0,0 +1,13 @@
+package com.codapt.uncle_blob_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UncleBlobServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UncleBlobServerApplication.class, args);
+	}
+
+}
---
+++ uncle-blob-server/src/test/java/com/codapt/uncle_blob_server/UncleBlobServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codapt.uncle_blob_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UncleBlobServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
