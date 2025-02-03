d275c82eb35238972a9d338d95b8e6e841e66c83
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-02-04 01:37:03 +0200

    adds api

--- uncle-blob-client/src/lib/api/init.api.ts
+++ uncle-blob-client/src/lib/api/init.api.ts
@@ -1,4 +1,5 @@
 import { config } from "dotenv"
+import { FileUpload } from "../types/api.types";
 
 config();
 
@@ -7,4 +8,22 @@ const BACKEND_URL = process.env.BACKEND_URL ?? "http://localhost:8888";
 export function getSwaggerUrl() {
     const endpoint = "/swagger-ui.html";
     return BACKEND_URL + endpoint;
+}
+
+/** Returns the base address of the backend api
+ * e.g `https://api.uncleblob.com` or  `http://localhost:8888`
+ */
+export function getBackendUrl() {
+    return BACKEND_URL;
+}
+
+export async function getAllFileUploads() : Promise<FileUpload[]> {
+    
+    const endpoint = "/api/v1/files";
+    const reqUrl = getBackendUrl() + endpoint;
+
+    const apiRes = await fetch(reqUrl);
+    const fileUploads = await apiRes.json() as FileUpload[];
+
+    return fileUploads;
 }
---
+++ uncle-blob-client/src/lib/types/api.types.ts
@@ -0,0 +1,11 @@
+export type FileUpload = {
+
+    uuid: string,
+    fileName: string,
+    oldFileName: string,
+    fileSize: number,
+    contentType: string,
+    updatedAt: Date,
+    lastUpdatedAt: Date
+
+}
