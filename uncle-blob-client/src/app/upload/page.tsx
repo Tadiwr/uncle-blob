f7a68a3dacc4c7a4cefdb2373b5a37f0ebf4adb7
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-02-04 10:54:07 +0200

    adds file controller

--- uncle-blob-client/src/app/upload/page.tsx
+++ uncle-blob-client/src/app/upload/page.tsx
@@ -1,17 +1,34 @@
 "use client";
 
-import React from 'react'
+import React, { ChangeEvent, FormEvent, useState } from 'react'
 import PrimaryButton from '../components/buttons/PrimaryButton';
 
 export default function UploadPage() {
+
+    const [file, setFile] = useState<File>();
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        console.log(file);
+        
+    }
+
+    const onFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const files = e.target.files;
+
+        if (files) {   
+            setFile(files[0]);
+        }
+    }
+
   return (
     <div className='p-5 flex flex-col items-center  h-full justify-center w-full' >
         <h1 className='mt-10 text-3xl font-medium' >Upload File</h1>
 
-        <form action="" className='rounded-xl p-5 w-4/5 md:w-1/3 grid grid-cols-1 items-center justify-center' >
+        <form onSubmit={handleSubmit} className='rounded-xl p-5 w-4/5 md:w-1/3 grid grid-cols-1 items-center justify-center' >
 
-            <input name='file_name' type="text" placeholder='File Name' />
-            <input name="file" type="file" placeholder='File Name' />
+            <input required onChange={onFileChange} type="file" placeholder='File Name' />
 
             <PrimaryButton className='w-full p-[10px] my-[5px]' >Upload</PrimaryButton>
 
