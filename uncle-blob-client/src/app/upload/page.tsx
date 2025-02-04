93ba5a9e6e341bf20848cd946089eb293ae49920
Tadiwanashe Shangwa <tshxng@gmail.com>
2025-02-04 10:34:47 +0200

    adds upload page ui

--- uncle-blob-client/src/app/upload/page.tsx
+++ uncle-blob-client/src/app/upload/page.tsx
@@ -6,14 +6,14 @@ import PrimaryButton from '../components/buttons/PrimaryButton';
 export default function UploadPage() {
   return (
     <div className='p-5 flex flex-col items-center  h-full justify-center w-full' >
-        <h1>Upload File</h1>
+        <h1 className='mt-10 text-3xl font-medium' >Upload File</h1>
 
-        <form action="" className='bg-slate-100 rounded-xl p-5 w-4/5 md:w-1/3 h-fit flex flex-col' >
+        <form action="" className='rounded-xl p-5 w-4/5 md:w-1/3 grid grid-cols-1 items-center justify-center' >
 
             <input name='file_name' type="text" placeholder='File Name' />
             <input name="file" type="file" placeholder='File Name' />
 
-            <PrimaryButton className='w-full' >Upload</PrimaryButton>
+            <PrimaryButton className='w-full p-[10px] my-[5px]' >Upload</PrimaryButton>
 
         </form>
     </div>
